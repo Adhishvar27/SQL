af15663dd768b9ecaa900476969f1c717c0d6c7d
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-05 20:36:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,35 @@ const express=require('express');
 const app=express();
 const mysql=require('mysql2');
 
+const connection= mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'testdatabase'
+});
 
+connection.connect((err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('Connection created successfully');
+
+    const creationQuery=`create table students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    std VARCHAR(5) NOT NULL
+    )`
+
+    connection.execute((creationQuery),(err)=>{
+        if(err){
+            console.log(err);
+            connection.end();
+            return;
+        }
+        console.log('query executed successfully');
+    })
+})
 
 app.use('/',(req,res)=>{
     res.send('Hello World');
