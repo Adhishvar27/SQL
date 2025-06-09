a6ec602283f17d59671725f45ed54936e58f3cf8
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-09 14:06:40 +0530

    Create students.js

---
+++ Student_database/models/students.js
@@ -0,0 +1,27 @@
+const {Sequelize,DataTypes}=require('sequelize');
+
+const sequelize=require('../dataBase/db');
+
+const students=sequelize.define('Students',{
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    age:{
+        type:DataTypes.INTEGER,
+        allowNull:false
+    }
+
+})
+
+module.exports=students;
