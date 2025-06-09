cf7006fb8ec7f8af1e87e653210e5998402e1bc3
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-09 14:08:20 +0530

    Add files via upload

--- Student_database/controllers/studentcontroller.js
+++ Student_database/controllers/studentcontroller.js
@@ -1,85 +1,169 @@
 const db=require('../dataBase/db');
-const adduser=(req,res)=>{
+const Student=require('../models/students');
+const adduser=async(req,res)=>{
+
+    try {
     const {name,email,age}=req.body;
-    if(!name || !email || !age){
-       return res.status(404).send('Enter the all values');
+    if(!name||!email||!age){
+        res.status(500).send(`Enter all the values`);
+    }
+    const insertVal=await Student.create({
+        name:name,
+        email:email,
+        age:age
+    });
+
+    res.status(201).send(`the user ${name} is added to database`);
+    } catch (error) {
+        res.status(500).send(error);
     }
 
-    const insertQuery=`INSERT INTO students (name,email,age) VALUES (?,?,?)`
-    db.execute(insertQuery,[name,email,age],(err)=>{
-        if(err) return res.status(500).send(err);
-        console.log('Query executed successfuly');
-        res.status(200).send(`New user ${name} has added to database`);
-    })
+
+    // const {name,email,age}=req.body;
+    // if(!name || !email || !age){
+    //    return res.status(404).send('Enter the all values');
+    // }
+
+    // const insertQuery=`INSERT INTO students (name,email,age) VALUES (?,?,?)`
+    // db.execute(insertQuery,[name,email,age],(err)=>{
+    //     if(err) return res.status(500).send(err);
+    //     console.log('Query executed successfuly');
+    //     res.status(200).send(`New user ${name} has added to database`);
+    // })
 }
 
-const getuser=(req,res)=>{
-    const id=req.query.id;
-    
-    let selectQuery=`SELECT * FROM students`
-    const val=[]
-    if(id){
-        selectQuery+=` WHERE id = ?`
-        val.push(id);
-    }
-    db.execute(selectQuery,val,(err,result)=>{
-        if(err) return res.status(500).send(err);
-        console.log('Query executed successfully');
-        if(result.length===0){
-            return res.status(200).send('No records found');
+const getuser=async(req,res)=>{
+
+    try {
+        const id=req.query.id;
+        const condition={};
+        if(id){
+            condition.id=id;
         }
-        res.status(200).send(result)
-    })
+        const selectusers=await Student.findAll({
+            where:condition
+        });
+
+        if(selectusers.length===0){
+            return res.status(200).send(`No records have been found`);
+        }
+        
+        res.status(200).send(selectusers);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+    // const id=req.query.id;
+    // let selectQuery=`SELECT * FROM students`
+    // const val=[]
+    // if(id){
+    //     selectQuery+=` WHERE id = ?`
+    //     val.push(id);
+    // }
+    // db.execute(selectQuery,val,(err,result)=>{
+    //     if(err) return res.status(500).send(err);
+    //     console.log('Query executed successfully');
+    //     if(result.length===0){
+    //         return res.status(200).send('No records found');
+    //     }
+    //     res.status(200).send(result)
+    // })
 
 }
 
-const updateuser=(req,res)=>{
+const updateuser=async (req,res)=>{
+    try {
     const id=req.params.id;
     const {name,email,age}=req.body;
-
-    const arr=[]
-    let updateQuerry=`UPDATE students SET `
+    const condition={};
     if(name){
-        updateQuerry+=`name=? `
-        arr.push(name);
+        condition.name=name;
     }
     if(email){
-        if(name){
-            updateQuerry+= ` ,`
-        }
-        updateQuerry+=`email=?`
-        arr.push(email);
+        condition.email=email;
     }
     if(age){
-        if(name || email){
-            updateQuerry+= ` ,`
-        }
-        updateQuerry+=`age=?`
-        arr.push(age);
+        condition.age=age;
     }
-    updateQuerry+=` WHERE id = ?`
-    arr.push(id);
 
-    db.execute(updateQuerry,arr,(err)=>{
-        if(err) return res.status(500).send(err);
-        console.log('Query executed successfully');
-        res.status(200).send(`the values has been updated for ${id}`);
+    const updatevalue=await Student.update(condition,{
+        where:{
+            id:id
+        }
     })
+
+    if(updatevalue[0]===0){
+        return res.status(200).send(`No records have been changed`);
+    }
+    
+    res.status(200).send(`Update Query is performed successfully`);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    
+
+    // const id=req.params.id;
+    // const {name,email,age}=req.body;
+
+    // const arr=[]
+    // let updateQuerry=`UPDATE students SET `
+    // if(name){
+    //     updateQuerry+=`name=? `
+    //     arr.push(name);
+    // }
+    // if(email){
+    //     if(name){
+    //         updateQuerry+= ` ,`
+    //     }
+    //     updateQuerry+=`email=?`
+    //     arr.push(email);
+    // }
+    // if(age){
+    //     if(name || email){
+    //         updateQuerry+= ` ,`
+    //     }
+    //     updateQuerry+=`age=?`
+    //     arr.push(age);
+    // }
+    // updateQuerry+=` WHERE id = ?`
+    // arr.push(id);
+
+    // db.execute(updateQuerry,arr,(err)=>{
+    //     if(err) return res.status(500).send(err);
+    //     console.log('Query executed successfully');
+    //     res.status(200).send(`the values has been updated for ${id}`);
+    // })
 
 }
 
-const deleteuser=(req,res)=>{
-    const id=req.params.id;
+const deleteuser=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const deletevalue=await Student.destroy({
+            where:{
+                id:id
+            }
+        })
 
-    const deleteQuery=`DELETE FROM students WHERE id = ?`;
-    db.execute(deleteQuery,[id],(err,result)=>{
-        if(err) return res.status(404).send(err);
-        console.log('Query executed successfully');
-        if(result.affectedRows===0){
-            return res.status(404).send(`No user found in ${id}`);
+        if(deletevalue[0]===0){
+            res.status(404).send(`No records found on user id ${id}`);
         }
-        res.status(200).send('row deleted successfully');
-    })
+        res.status(200).send(`User has been deleted successfully`);
+    } catch (error) {
+        res.status(500).send('error');
+    }
+    // const id=req.params.id;
+
+    // const deleteQuery=`DELETE FROM students WHERE id = ?`;
+    // db.execute(deleteQuery,[id],(err,result)=>{
+    //     if(err) return res.status(404).send(err);
+    //     console.log('Query executed successfully');
+    //     if(result.affectedRows===0){
+    //         return res.status(404).send(`No user found in ${id}`);
+    //     }
+    //     res.status(200).send('row deleted successfully');
+    // })
 
 }
 
