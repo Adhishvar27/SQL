c807ab662309ab015b823ee325d2d5cb06cc96d6
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-09 14:07:13 +0530

    Add files via upload

--- Student_database/dataBase/db.js
+++ Student_database/dataBase/db.js
@@ -1,27 +1,72 @@
-const mysql=require('mysql2');
+const {Sequelize}= require('sequelize');
 
-const connection=mysql.createConnection({
+const sequelize=new Sequelize('students_database','root','root',{
     host:'localhost',
-    user:'root',
-    password:'root',
-    database:'students_database'
-})
+    dialect:'mysql'
+});
 
-connection.connect((err)=>{
-    if(err) return console.log(err);
-    console.log('connection made successfully');
+(async ()=>{try {
+    await sequelize.authenticate();
+    console.log('connection is made successfully');
+} catch (error) {
+    console.log(error);
+}})();
 
-    const createQuery=`create table IF NOT EXISTS students (
-    id int Primary Key Auto_Increment,
-    name VARCHAR(25),
-    email VARCHAR(50) Unique,
-    age INT
-    )`
+module.exports=sequelize;
 
-    connection.query(createQuery,(err)=>{
-        if(err) return console.log(err);
-        console.log('Query exeuted successfully');
-    })
-})
 
-module.exports=connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql=require('mysql2');
+
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'root',
+//     database:'students_database'
+// })
+
+// connection.connect((err)=>{
+//     if(err) return console.log(err);
+//     console.log('connection made successfully');
+
+//     const createQuery=`create table IF NOT EXISTS students (
+//     id int Primary Key Auto_Increment,
+//     name VARCHAR(25),
+//     email VARCHAR(50) Unique,
+//     age INT
+//     )`
+
+//     connection.query(createQuery,(err)=>{
+//         if(err) return console.log(err);
+//         console.log('Query exeuted successfully');
+//     })
+// })
+
+// module.exports=connection;
