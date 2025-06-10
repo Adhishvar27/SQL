b6115aae2a6f07142e407693be5159bca423dde9
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-10 20:39:13 +0530

    Create booking.js

---
+++ Bus_Booking_System/models/booking.js
@@ -0,0 +1,19 @@
+const {Sequelize,DataTypes}=require('sequelize');
+
+const db=require('../Create_DataBase_Querys/dataBase');
+
+const booking=db.define('booking',{
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        allowNull:false
+    },
+    seatNumber:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
+})
+
+console.log('Booking table is created successfully');
+
+module.exports=booking;
