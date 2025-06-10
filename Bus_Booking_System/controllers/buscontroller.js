ea5e650a58b438739d387f674abf1e397bfc8a2b
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-10 20:40:02 +0530

    Add files via upload

--- Bus_Booking_System/controllers/buscontroller.js
+++ Bus_Booking_System/controllers/buscontroller.js
@@ -1,37 +1,77 @@
+const {Op}=require('sequelize');
 const db=require('../Create_DataBase_Querys/dataBase');
+const BusDB=require('../models/bus');
 
-const addbus=(req,res)=>{
-    const {busNumber,totalSeats,availableSeats}=req.body;
-    if(!busNumber || !totalSeats || !availableSeats){
-        return res.status(404).send('Enter all details of the bus');
+const addbus=async (req,res)=>{
+    try {
+      const {busNumber,totalSeats,availableSeats}=req.body;
+    if(!busNumber||!totalSeats||!availableSeats){
+        return res.status(404).send('Enter all deatils');
     }
 
-    const insertQuery=`INSERT INTO buses (busNumber,totalSeats,availableSeats) VALUES (?,?,?)`;
-    db.execute(insertQuery,[busNumber,totalSeats,availableSeats],(err)=>{
-        if(err) return res.status(500).send(err);
-        console.log('Query executed successfully');
-        res.status(200).send(`the number ${busNumber} bus is added to the DB`);
-    })
+    const insertValue= await BusDB.create({
+        busNumber:busNumber,
+        totalSeats:totalSeats,
+        availableSeats:availableSeats
+    });   
+
+    res.status(201).send('Values inserted into Bus table successfully')
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+//     const {busNumber,totalSeats,availableSeats}=req.body;
+//     if(!busNumber || !totalSeats || !availableSeats){
+//         return res.status(404).send('Enter all details of the bus');
+//     }
+
+//     const insertQuery=`INSERT INTO buses (busNumber,totalSeats,availableSeats) VALUES (?,?,?)`;
+//     db.execute(insertQuery,[busNumber,totalSeats,availableSeats],(err)=>{
+//         if(err) return res.status(500).send(err);
+//         console.log('Query executed successfully');
+//         res.status(200).send(`the number ${busNumber} bus is added to the DB`);
+//     })
 }
 
-const retrivebus=(req,res)=>{
-    const number=req.query.aboveseats;
+const retrivebus=async (req,res)=>{
 
-    let selectQuery=`SELECT * FROM buses`
-    let arr=[]; 
-
-    if(number){
-        selectQuery+=` where availableSeats > ?`;
-        arr.push(number);
-    }
-    db.execute(selectQuery,arr,(err,result)=>{
-        if(err) return res.status(500).send(err);
-        if(result.length===0){
-            return res.status(200).send(`No buses avilable in ${number} seats`);
+    try {
+        const number=parseInt(req.query.aboveseats);
+        let condition={};
+        if(!isNaN(number)){
+            condition.availableSeats={
+                [Op.gt]:number
+            };
         }
-        res.status(200).send(result);
+        const SelectValues=await BusDB.findAll({
+            where:condition
+        });
 
-    })
+        // if(SelectValues.length===0){
+        //     return res.status(404).send('No records have been found');
+        // }
+
+        res.status(200).send(SelectValues);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    // const number=req.query.aboveseats;
+
+    // let selectQuery=`SELECT * FROM buses`
+    // let arr=[]; 
+
+    // if(number){
+    //     selectQuery+=` where availableSeats > ?`;
+    //     arr.push(number);
+    // }
+    // db.execute(selectQuery,arr,(err,result)=>{
+    //     if(err) return res.status(500).send(err);
+    //     if(result.length===0){
+    //         return res.status(200).send(`No buses avilable in ${number} seats`);
+    //     }
+    //     res.status(200).send(result);
+
+    // })
 }
 
 module.exports={
--- Bus_Booking_System/controllers/userscontroller.js
+++ Bus_Booking_System/controllers/userscontroller.js
@@ -1,28 +1,51 @@
 const db=require('../Create_DataBase_Querys/dataBase');
+const UserDB=require('../models/users');
 
-const adduser=(req,res)=>{
-    const {name,email}=req.body;
-    if(!name || !email){
-        return res.status(404).send('Enter the values correctly');
+const adduser=async (req,res)=>{
+    try {
+        const {name,email}=req.body;
+        if(!name||!email){
+            return res.status(400).send('enter all details');
+        }
+        const insertValues=await UserDB.create({
+            name:name,
+            email:email
+        });
+
+        res.status(201).send('user deatails has been added to user table');
+    } catch (error) {
+        res.status(500).send(error);
     }
+    // const {name,email}=req.body;
+    // if(!name || !email){
+    //     return res.status(404).send('Enter the values correctly');
+    // }
     
-    const insertQuery=`INSERT INTO users (name,email) VALUES (?,?)`;
+    // const insertQuery=`INSERT INTO users (name,email) VALUES (?,?)`;
 
-    db.execute(insertQuery,[name,email],(err)=>{
-        if(err) return res.status(500).send(err);
-        console.log('Query executed successfully');
-        res.status(200).send(`the user ${name} is added succesfully`);
-    })
-
-
+    // db.execute(insertQuery,[name,email],(err)=>{
+    //     if(err) return res.status(500).send(err);
+    //     console.log('Query executed successfully');
+    //     res.status(200).send(`the user ${name} is added succesfully`);
+    // })
 }
-const selectuser=(req,res)=>{
-    const selectQuery=`SELECT * FROM users`
-    db.execute(selectQuery,(err,result)=>{
-        if(err) return res.status(500).send(err);
-        console.log('Query executed successfully');
-        res.status(200).send(result);
-    });
+const selectuser=async(req,res)=>{
+
+    try {
+        const selectValues=await UserDB.findAll();
+        if(selectValues.length===0){
+            return res.status(200).send('currently no user persent in the database');
+        }
+        res.status(200).send(selectValues);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    // const selectQuery=`SELECT * FROM users`
+    // db.execute(selectQuery,(err,result)=>{
+    //     if(err) return res.status(500).send(err);
+    //     console.log('Query executed successfully');
+    //     res.status(200).send(result);
+    // });
 }
 
 module.exports={
