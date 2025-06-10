79ced15dd4388b0251fca3fe1d5e7b336897241b
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-10 20:40:35 +0530

    Add files via upload

--- Bus_Booking_System/Create_DataBase_Querys/dataBase.js
+++ Bus_Booking_System/Create_DataBase_Querys/dataBase.js
@@ -1,65 +1,84 @@
-const mysql=require('mysql2');
+const {Sequelize}=require('sequelize');
 
-const connection=mysql.createConnection({
+const sequelize=new Sequelize('bus_system','root','root',{
     host:'localhost',
-    user:'root',
-    password:'root',
-    database:'bus_system'
+    dialect:'mysql'
 });
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
+(async ()=>{try {
+    await sequelize.authenticate();
     console.log('connection made successfully');
+} catch (error) {
+    console.log(error);    
+}})();
 
-    const createQuery1=`create table IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    email VARCHAR(50)
-    )`
+module.exports=sequelize;
 
-    const createQuery2=`create table IF NOT EXISTS buses(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    busNumber VARCHAR(10),
-    totalSeats INT,
-    availableSeats INT
-    )`
 
-    const createQuery3=`create table IF NOT EXISTS booking(
-    id INT PRIMARY KEY,
-    seatNumber VARCHAR(5)
-    )`
 
-    const createQuery4=`create table IF NOT EXISTS payments(
-    id INT PRIMARY KEY,
-    amountPaid INT,
-    paymentStatus VARCHAR(10)
-    )`
 
-    connection.query(createQuery1,(err)=>{
-        if(err){
-           return console.log('Error in Query 1 : ',err);
-        }
-        console.log('User table is created');
+// const mysql=require('mysql2');
 
-        connection.query(createQuery2,(err)=>{
-            if(err) return console.log('Error in query 2 : ', err);
-            console.log('Buses table is created');
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'root',
+//     database:'bus_system'
+// });
 
-                connection.query(createQuery3,(err)=>{
-                    if(err) return console.log('Error in Query 3 : ', err);
-                    console.log('Booking table is created');
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log('connection made successfully');
 
-                        connection.query(createQuery4,(err)=>{
-                            if(err) return console.log('Error in Query 4 : ', err);
-                            console.log('Payment table is created');
-                        })
-                })
-        })
-    })
+//     const createQuery1=`create table IF NOT EXISTS users(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(50),
+//     email VARCHAR(50)
+//     )`
 
-})
+//     const createQuery2=`create table IF NOT EXISTS buses(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     busNumber VARCHAR(10),
+//     totalSeats INT,
+//     availableSeats INT
+//     )`
 
-module.exports=connection;
+//     const createQuery3=`create table IF NOT EXISTS booking(
+//     id INT PRIMARY KEY,
+//     seatNumber VARCHAR(5)
+//     )`
+
+//     const createQuery4=`create table IF NOT EXISTS payments(
+//     id INT PRIMARY KEY,
+//     amountPaid INT,
+//     paymentStatus VARCHAR(10)
+//     )`
+
+//     connection.query(createQuery1,(err)=>{
+//         if(err){
+//            return console.log('Error in Query 1 : ',err);
+//         }
+//         console.log('User table is created');
+
+//         connection.query(createQuery2,(err)=>{
+//             if(err) return console.log('Error in query 2 : ', err);
+//             console.log('Buses table is created');
+
+//                 connection.query(createQuery3,(err)=>{
+//                     if(err) return console.log('Error in Query 3 : ', err);
+//                     console.log('Booking table is created');
+
+//                         connection.query(createQuery4,(err)=>{
+//                             if(err) return console.log('Error in Query 4 : ', err);
+//                             console.log('Payment table is created');
+//                         })
+//                 })
+//         })
+//     })
+
+// })
+
+// module.exports=connection;
