cc54f9ff7d4984f1684606d10bc8463c87421247
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-17 18:40:04 +0530

    Add files via upload

--- Bus_Booking_System/main.js
+++ Bus_Booking_System/main.js
@@ -1,10 +1,7 @@
 const express=require('express');
 const app=express();
 const db=require('./Create_DataBase_Querys/dataBase');
-const userModels=require('./models/users');
-const busModels=require('./models/bus');
-const paymnetModels=require('./models/payment');
-const bookingModels=require('./models/booking');
+require('./models');
 app.use(express.json());
 
 const usersrouters=require('./routers/usersrouter');
@@ -13,11 +10,14 @@ app.use('/users',usersrouters);
 const busrouters=require('./routers/busrouter');
 app.use('/bus',busrouters);
 
+const bookingrouter=require('./routers/bookingrouter');
+app.use('/booking',bookingrouter);
+
 app.use('/home',(req,res)=>{
     res.send('Hello welcome to bus ticket booking system');
 })
 
-db.sync({force:true}).then(()=>{
+db.sync({}).then(()=>{
     app.listen(3000,()=>{
     console.log('Server is running');
 })
